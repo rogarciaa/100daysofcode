678448c995b6dcbc3321f8084002cfca74dbb5fb
rogarciaa <rodrigogaga@gmail.com>
2022-02-07 23:33:08 -0300

    added folder project

---
+++ band-name-generator-end/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the band name generator!\n")
+
+born_city = input("Which city you grew up in?: ")
+
+pet_name = input("What is the name of your pet?: ")
+
+print("Your band name could be " + born_city + " " + pet_name )
