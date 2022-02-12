c41b7595caf625c682959d82e062d36b7305f59b
rogarciaa <rodrigogaga@gmail.com>
2022-02-12 03:20:47 -0300

    improved conditionals

--- 004-rock-paper-scissors/main.py
+++ 004-rock-paper-scissors/main.py
@@ -31,15 +31,14 @@ ascii_draw_list=[rock, paper, scissors]
 user_choice=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
 computer_choice=random.randint(0, 2)
 
-if (user_choice == 0 and computer_choice == 2) or (user_choice == 1 and computer_choice == 0) or (user_choice == 2 and computer_choice == 1):
-  print(f"Your choice:{ascii_draw_list[user_choice]}\n, Computer choice{ascii_draw_list[computer_choice]}\n")
-  print("You Win!")
-elif (user_choice == 0 and computer_choice == 1) or (user_choice == 1 and computer_choice == 2) or (user_choice == 2 and computer_choice == 0):
-  print(f"Your choice:{ascii_draw_list[user_choice]}\n, Computer choice{ascii_draw_list[computer_choice]}\n")
-  print("Game Over!")
-elif user_choice >= 3 or user_choice < 0:
+if user_choice >= 3 or user_choice < 0:
   print("You input a invalid number")
 else:
-  print(f"Your choice:{ascii_draw_list[user_choice]}\n, Computer choice{ascii_draw_list[computer_choice]}\n")
-  print("It's a draw!")
+  print(f"Your choice:{ascii_draw_list[user_choice]}\nComputer choice{ascii_draw_list[computer_choice]}\n")
+  if (user_choice == 0 and computer_choice == 2) or (user_choice == 1 and computer_choice == 0) or (user_choice == 2 and computer_choice == 1):
+    print("You Win!")
+  elif (user_choice == 0 and computer_choice == 1) or (user_choice == 1 and computer_choice == 2) or (user_choice == 2 and computer_choice == 0):
+    print("Game Over!")
+  else:
+    print("It's a draw!")
 
